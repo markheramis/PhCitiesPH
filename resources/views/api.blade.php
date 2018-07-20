4c947994eade676e639f009e7c2bd2df95da7779
Mark Heramis <chumheramis@gmail.com>
2018-07-20 18:26:08 +0800

    API documentation updated
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -16,11 +16,15 @@
     <body>
         <a href="https://github.com/halimus/laravel-rest-api"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
         <div class="container" style="padding-bottom: 100px;">
-            <h2 style="text-align:center;"><u>A modern REST API in Laravel 5</u></h2>
+            <h2 style="text-align:center;"><u>A modern REST API in Laravel 5 for Philippine Places</u></h2>
             <div class="row">
                 <div class="col-md-6 col-sm-12" style="border-right:1px solid silver;">
-                    <p><br>
+                    <p>
                         The master project repository is
+                        <a href="https://github.com/NinongFoundation/PhCitiesPH" target="_blank">https://github.com/NinongFoundation/PhCitiesPH</a>
+                    </p>
+                    <p>
+                        This project is built from this repository
                         <a href="https://github.com/halimus/laravel-rest-api" target="_blank">https://github.com/halimus/laravel-rest-api</a>
                     </p>
                     <p>Click on the links to check simple <strong>GET methods</strong> whether is working.</p>
